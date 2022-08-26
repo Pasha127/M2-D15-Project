ae9278002b9be9bd73553238ae752ecb70af3c9b
Paul Levitsky <85572670+Pasha127@users.noreply.github.com>
2022-08-26 16:13:34 +0200

    initial commit

---
+++ M2-D15-Project/js/backOffice.js
@@ -0,0 +1,61 @@
+const titleInput = document.querySelector("#titleInput");
+const descriptionInput = document.querySelector("#descriptionInput");
+const genreInput = document.querySelector("#genreInput");
+const urlInput = document.querySelector("#urlInput");
+const addContentBtn = document.querySelector("#addContentBtn");
+const body = document.querySelector("body");
+
+const addContent = (content) => {
+ 
+  fetch(`https://striveschool-api.herokuapp.com/api/movies/`, {
+    method: "POST",
+    headers: {'Content-Type': 'application/json',"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzA3ODg0NDFlYjc2ZDAwMTUxNTAxZjgiLCJpYXQiOjE2NjE1MTI1MTgsImV4cCI6MTY2MjcyMjExOH0.YpsgajqhspK1z8A5EyOdHV6kTmpEm1iD9zreNQ2kmxU"}, 
+    body: JSON.stringify(content)
+  }).then(res => {
+    
+    console.log("Request complete! response:", res);
+  }).catch(err => console.log('post failed:', err));
+  clearInputs();  
+  createAlert("Submitted Item", "success");
+}
+
+const createAlert = (string,color) => {
+  const previousAlerts = document.querySelectorAll(".alertBar");
+  for(bar of previousAlerts){bar.remove()};
+  const alertContainer = document.createElement("div");
+  alertContainer.setAttribute("class", "container d-flex justify-content-center");
+  const alertBody = document.createElement("div");
+  alertBody.setAttribute("class", `alert alert-${color} alertBar`);
+  alertBody.setAttribute("role", "alert");
+  body.prepend(alertContainer);
+  alertContainer.append(alertBody);
+  alertBody.innerText = `${string}`;
+  setTimeout(function () {alertContainer.remove()}, 5000)
+
+}
+
+const gatherContent = ()=>{
+  const content = {name:titleInput.value, description:descriptionInput.value, category:genreInput.value, imageUrl: urlInput.value };
+  console.log(content)
+  addContent(content)
+}
+
+const clearInputs = ()=>{
+  titleInput.value = "";
+  descriptionInput.value = "";
+  genreInput.value = "";
+  urlInput.value = "";  
+}
+
+window.onload = () =>{
+  addContentBtn.addEventListener("click", gatherContent)
+  testFetch();
+}
+const testFetch = () =>{
+fetch("https://striveschool-api.herokuapp.com/api/movies/", {
+            method: "GET",
+            headers: {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzA3ODg0NDFlYjc2ZDAwMTUxNTAxZjgiLCJpYXQiOjE2NjE0MzgwMjAsImV4cCI6MTY2MjY0NzYyMH0.FxXNN1ADQHPQJbchifn_vi_cp1sdPcdONESnfaMV4DE"}})
+            
+            .then(res => res.json()).then(data => console.log(data))
+            .catch(err => console.log('post failed:', err));
+}
